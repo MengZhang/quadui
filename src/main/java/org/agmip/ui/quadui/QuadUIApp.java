6a4504dc8b0f6e2cd3571c9d571ec2b6293c196d
MengZhang <meng.zhang15@ufl.edu>
2013-05-06 12:14:49 -0400

    merge conflict

--- src/main/java/org/agmip/ui/quadui/QuadUIApp.java
+++ src/main/java/org/agmip/ui/quadui/QuadUIApp.java
@@ -5,16 +5,12 @@ import org.apache.pivot.collections.Map;
 import org.apache.pivot.wtk.Application;
 import org.apache.pivot.wtk.DesktopApplicationContext;
 import org.apache.pivot.wtk.Display;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class QuadUIApp extends Application.Adapter {
     private QuadUIWindow window = null;
-    private static Logger LOG = LoggerFactory.getLogger(QuadCmdLine.class);
 
     @Override
     public void startup(Display display, Map<String, String> props) throws Exception {
-        LOG.info("Run GUI start");
         BXMLSerializer bxml = new BXMLSerializer();
         window = (QuadUIWindow) bxml.readObject(getClass().getResource("/quadui.bxml"));
         window.open(display);
@@ -37,12 +33,10 @@ public class QuadUIApp extends Application.Adapter {
             }
         }
         if (cmdFlg) {
-            LOG.info("Run CLI");
             QuadCmdLine cmd = new QuadCmdLine();
             cmd.run(args);
             
         } else {
-            LOG.info("Run GUI");
             DesktopApplicationContext.main(QuadUIApp.class, args);
         }
     }
